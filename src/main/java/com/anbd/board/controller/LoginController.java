9a6dd8ee21e07888a7abdc6b1ec11346e2006a45
jang <clduddkwk@naver.com>
2022-01-10 16:57:30 +0900

    2022 01 10 16:57 [로그인, 아이디찾기, 비밀번호 찾기 기능 구현]

--- src/main/java/com/anbd/board/controller/LoginController.java
+++ src/main/java/com/anbd/board/controller/LoginController.java
@@ -1,22 +1,27 @@
 package com.anbd.board.controller;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.Map;
 import java.util.Optional;
 
-import javax.print.attribute.HashAttributeSet;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.context.Context;
 
 import com.anbd.board.Hash;
+import com.anbd.board.RandomPassword;
 import com.anbd.board.entity.ClientEntity;
 import com.anbd.board.model.Client;
 import com.anbd.board.repository.ClientRepository;
@@ -24,12 +29,16 @@ import com.anbd.board.service.ClientService;
 
 @Controller
 public class LoginController {
-
+	
+	@Autowired
+	public JavaMailSender javaMailSender;
+	
 	@Autowired
 	ClientRepository repository;
 	
 	@Autowired
 	ClientService service;
+	
 	
 	// 회원가입 등록
 	@RequestMapping(value = "/anbd/register", method =  RequestMethod.POST)
@@ -90,20 +99,92 @@ public class LoginController {
 			HttpSession session = request.getSession();
 		if (password.equals(user.getClient_password())) {
 			session.setAttribute("client", service.toDto(client.get()));
-			return "redirect:/anbd/main";
+			return "redirect:/anbd";
 		}
-			String message = "로그인 정보가 다릅니다.";
+			String message = "비밀번호가 일치하지 않습니다";
 			model.addAttribute("message", message);
 			model.addAttribute("url", "login");
 			return "alertLogin";
 		} else {
-			String message = "로그인 정보가 다릅니다.";
+			String message = "아이디가 존재하지 않습니다";
 			model.addAttribute("message", message);
 			model.addAttribute("url", "login");
 			return "alertLogin";
 		}
 	}	
-		
+	
+	// 아이디 찾기 페이지
+		@RequestMapping(value = "/anbd/find_id")
+		public String find_id() {
+			return "find_id";
+		}
+	// 아이디 찾기 페이지
+		@RequestMapping(value = "/anbd/find_id_C")
+		public String find_id_C() {
+			return "find_id";
+		}
+	// 아이디 찾기 체크
+	@RequestMapping(value = "/anbd/findId", method = RequestMethod.POST)
+	public String find_password(String client_name, String client_email, String client_tel, Model model) {
+		Optional<ClientEntity> client = repository.id_find(client_name, client_email, client_tel);
+		if (client.isPresent()) {
+			Client user = null;
+			user = service.toDto(client.get());
+			model.addAttribute("client", user);
+			return "find_id_C";
+		}
+		return "redirect:/anbd/find_id";
+	}	
+	// 비밀번호 찾기
+	@RequestMapping(value = "/anbd/find_password")
+	public String find_password() {
+		return "find_password";
+	}
+	
+	// 비밀번호 찾기 이메일 체크
+	@RequestMapping(value = "/anbd/find_password_C", method = RequestMethod.POST)
+	public String pw_find(String client_id, String client_email, Model model,Map<String, Object> variables) throws Exception  {
+        Optional<ClientEntity> pwFindCheck = repository.userEmailCheck(client_id,client_email);
+        Context context = new Context();
+        context.setVariables(variables);
+        String message;
+        if(pwFindCheck.isPresent()) {
+            
+            String From= "clduddkwk@naver.com";
+            String To= client_email;
+            
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, "UTF-8");
+
+            // 임시비밀번호 DB 저장
+        	RandomPassword password = new RandomPassword();
+        	String newPassword = password.password();
+        	Hash hash = new Hash();
+        	String changePassword = hash.hashtest(newPassword);
+        	Client client = service.toDto(repository.getById(client_id));
+			client.setClient_password(changePassword);
+			repository.save(service.toEntity(client));
+			
+			messageHelper.setFrom(From);
+            messageHelper.setTo(To);
+            messageHelper.setSubject("[아나바다 마켓] 임시비밀번호 발급입니다.");
+            messageHelper.setText(client_id+"님 안녕하세요.<br>"+"[아나바다 마켓] 임시비밀번호 발급입니다." + "<br>" + 
+            					"로그인 하셔서 마이페이지에서 비밀번호 변경해주세요!<br>"+
+            					"임시비밀번호: " +newPassword, true);
+            javaMailSender.send(mimeMessage);
+            
+        	message = "회원님의 이메일로 임시 비밀번호를 전송하였습니다.";
+        	model.addAttribute("message", message);
+			model.addAttribute("url", "login");
+        	return "alertLogin";
+        } else {
+        	message = "회원정보가 일치하지 않습니다";
+			model.addAttribute("message", message);
+			model.addAttribute("url", "find_password");
+			return "alertLogin";
+        }
+    }
+	
 	// 아이디 중복체크
 	@SuppressWarnings("unchecked")
 	@ResponseBody
--- src/main/java/com/anbd/board/repository/ClientRepository.java
+++ src/main/java/com/anbd/board/repository/ClientRepository.java
@@ -20,4 +20,14 @@ public interface ClientRepository extends JpaRepository<ClientEntity, String>{
 	// 이메일 중복검사(비밀번호 찾을 때)
 	@Query("select c from ClientEntity c where c.client_email = :client_email")
 	Optional<ClientEntity> checkEmail(@Param("client_email")String client_email);
+	
+	// 아이디 찾기
+	@Query("select c from ClientEntity c where c.client_name = :client_name and c.client_email = :client_email and c.client_tel = :client_tel")
+	Optional<ClientEntity> id_find(@Param("client_name") String client_name, @Param("client_email") String client_email, @Param("client_tel") String client_tel);
+	
+	// 비밀번호 찾기
+	@Query("select c from ClientEntity c where c.client_id = :client_id and c.client_email = :client_email")
+	Optional<ClientEntity> userEmailCheck(@Param("client_id") String client_id, @Param("client_email") String client_email);
+	
+	
 }
