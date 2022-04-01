d34d2895e214aa8166c7244aad362e4c6be8f25e
jang <clduddkwk@naver.com>
2022-04-01 19:38:50 +0900

    배포

--- src/main/java/com/anbd/board/AnbdApplication.java
+++ src/main/java/com/anbd/board/AnbdApplication.java
@@ -4,11 +4,17 @@ package com.anbd.board;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @EnableAutoConfiguration
 @SpringBootApplication
-public class AnbdApplication {
+public class AnbdApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AnbdApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnbdApplication.class, args);
